40daf1d7f40fe3901c0099b1fff4312313207556
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2018-03-18 12:11:07 +0100

    Replace new line characters in the Whiteparse grammar shell

--- Whiteparse.Examples/Program.cs
+++ Whiteparse.Examples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Sprache;
 using Whiteparse.Grammar;
 
@@ -9,27 +10,50 @@ namespace Whiteparse.Examples
         private static void Main()
         {
             Console.WriteLine("Whiteparse Grammar Shell");
+            Console.WriteLine("- Write 'exit' to leave this shell.");
+            Console.WriteLine("- '\\n' will be replaced with a new line character, unless escaped.");
+            Console.WriteLine();
 
             while (true)
             {
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("> ");
-                string line = Console.ReadLine();
+
+                string line = null;
+                try
+                {
+                    line = Console.ReadLine() ?? "";
+
+                    // replace '\n' (but not '\\n') and remove the trailing backslash of escaped control characters
+                    line = Regex.Replace(line, @"(?<!\\)\\n", "\n").Replace("\\\\n", "\\n");
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine();
+                    Console.WriteLine(line);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Gray;
 
-                if (line == "exit")
-                    break;
+                if (line != null)
+                {
+                    if (line == "exit")
+                        break;
 
-                try
-                {
-                    Specification spec = Specification.FromString(line);
-                    Console.WriteLine(spec);
-                }
-                catch (ParseException e)
-                {
-                    Console.WriteLine(e.Message);
+                    try
+                    {
+                        Specification spec = Specification.FromString(line);
+                        Console.WriteLine(spec);
+                    }
+                    catch (ParseException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
                 }
             }
         }
